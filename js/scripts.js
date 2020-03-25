ac1140b9950160f8ba0d4fafa2d502e979d39357
Felix <felixatika@gmail.com>
2020-03-25 06:16:10 -0400

    add division sign

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ var add = function(number1, number2){
   alert(multiply(number1,number2));
   
   var divide = function(number1, number2){
-  return number1-number2;
+  return number1/number2;
   };
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 =parseInt( prompt("Enter another number:"));
